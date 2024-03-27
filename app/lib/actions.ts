a1cbf4c7a09a84c65bf25e71b8161fe0f8aef167
Abdul Sammi Gul <sammigul11005@gmail.com>
2024-03-27 14:55:53 +0500

    Add server side form validation

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -69,12 +69,24 @@ export async function createInvoice(prevState: State, formData: FormData) {
 
 const UpdateInvoice = FormSchema.omit({id: true, date:true});
 
-export async function updateInvoice(id: string, formData: FormData) {
-    const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice( id: string,prevState: State, formData: FormData) {
+
+    // Validate form using Zod
+    const validatedFields = UpdateInvoice.safeParse({
       customerId: formData.get('customerId'),
       amount: formData.get('amount'),
       status: formData.get('status'),
     });
+   
+    // If form validation fails, return errors early. Otherwise, continue.
+    if (!validatedFields.success) {
+      return {
+        errors: validatedFields.error.flatten().fieldErrors,
+        message: 'Missing Fields. Failed to Update Invoice.',
+      };
+    }
+
+    const {customerId, amount, status} = validatedFields.data;
     const amountInCents = amount * 100;
     try{
       await sql`
@@ -94,7 +106,6 @@ export async function updateInvoice(id: string, formData: FormData) {
 
 
 export async function deleteInvoice(id: string){
-  throw new Error('Failed to delete invoice');
   try{
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
